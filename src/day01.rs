426b35a494464238ea218f5d014b07aaca698628
Jim Gartland <jim@danube.io>
2024-10-20 16:22:44 +0100

    Day 1-2

--- src/day01.rs
+++ src/day01.rs
@@ -1,19 +1,62 @@
+use std::collections::HashMap;
+
 pub fn day01(input: Vec<String>) -> (String, String) {
+    let lookup_1 = HashMap::from([
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+    ]);
+    let lookup_2 = HashMap::from([
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+    (
+        format!("{}", solve(&input, lookup_1)),
+        format!("{}", solve(&input, lookup_2)),
+    )
+}
+
+fn solve(input: &Vec<String>, lookup: HashMap<&str, i32>) -> u32 {
     let mut sum = 0;
     for line in input {
         let mut digits = Vec::new();
-        for character in line.chars() {
-            if character.is_numeric() {
-                digits.push(character);
+
+        for index in 0..line.len() {
+            for entry in lookup.keys() {
+                if line.len() >= index + entry.len() && &&line[index..index + entry.len()] == entry
+                {
+                    digits.push(lookup.get(entry).unwrap());
+                }
             }
         }
+
         sum += format!("{}{}", digits.first().unwrap(), digits.last().unwrap())
             .parse::<u32>()
             .unwrap();
     }
-    let answer1 = sum;
-    let answer2 = "TODO".to_string();
-    (format!("{}", answer1), format!("{}", answer2))
+    sum
 }
 
 #[cfg(test)]
@@ -31,6 +74,23 @@ mod tests {
             ])
             .0,
             "142".to_string()
+        )
+    }
+
+    #[test]
+    fn day01_2() {
+        assert_eq!(
+            day01(vec![
+                "nqninenmvnpsz874".to_string(),
+                "8twofpmpxkvvdnpdnlpkhseven4ncgkb".to_string(),
+                "six8shdkdcdgseven8xczqrnnmthreecckfive".to_string(),
+                "qlcnz54dd75nine7jfnlfgz".to_string(),
+                "7vrdhggdkqbnltlgpkkvsdxn2mfpghkntzrhtjgtxr".to_string(),
+                "cdhmktwo6kjqbprvfour8".to_string(),
+                "ninekkvkeight9three".to_string(),
+            ])
+            .1,
+            "493".to_string()
         )
     }
 }
