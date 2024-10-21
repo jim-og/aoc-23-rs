c3aa6257532a5df88b782a4950c6c9a5d7b1c199
Jim Gartland <jim@danube.io>
2024-10-21 17:09:44 +0100

    Day 3

--- src/day03.rs
+++ src/day03.rs
@@ -1,62 +1,98 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-pub fn day03(input: Vec<String>) -> (String, String) {
-    // Find coordinates of symbols
-    let mut symbol_coords = HashSet::new();
-    for (row, line) in input.iter().enumerate() {
-        for (col, c) in line.chars().enumerate() {
-            if !c.is_numeric() && c != '.' {
-                symbol_coords.insert(format!("{}{}", row, col));
-            }
-        }
-    }
+// plan
+// iterate over each character in input
+// decide whether it is a period, a part, or a symbol
+// store each point in a hashmap with key (row, col) and it's value
+// value should be an enum which is either Period, Symbol(char) or Part(number)
+// number should be the full part number, not just the number at that point
 
-    // Find numbers and check for an adjacent symbol
-    let mut sum = 0;
-    for (row, line) in input.iter().enumerate() {
-        let mut part_number = "".to_string();
-        let mut adjacent_symbol = false;
-
-        for (col, c) in line.chars().enumerate() {
-            if c.is_numeric() {
-                // Number found
-                part_number.push(c);
-                // Check adjacent coords if not found already
-                adjacent_symbol =
-                    adjacent_symbol || check_adjacent(row as i32, col as i32, &symbol_coords)
-            } else if !part_number.is_empty() {
-                if adjacent_symbol {
-                    sum += part_number
-                        .parse::<u32>()
-                        .expect("Unable to parse part number");
-                }
-                part_number.clear();
-                adjacent_symbol = false;
-            }
-        }
-
-        // Handle a number at the end of a line
-        if !part_number.is_empty() && adjacent_symbol {
-            sum += part_number
-                .parse::<u32>()
-                .expect("Unable to parse part number");
-        }
-    }
-
-    let answer_2 = "ANSWER_2".to_string();
-    (format!("{}", sum), format!("{}", answer_2))
+enum Point {
+    Period,
+    Symbol(char),
+    Part(u32),
 }
 
-fn check_adjacent(row: i32, col: i32, symbol_coords: &HashSet<String>) -> bool {
-    for r in row - 1..row + 2 {
-        for c in col - 1..col + 2 {
-            let coord = format!("{}{}", r, c);
-            if symbol_coords.contains(&coord) {
-                return true;
+fn process_part(
+    schematic: &mut HashMap<(usize, usize), Point>,
+    row: usize,
+    col: usize,
+    part: &mut Vec<char>,
+) {
+    let number = part
+        .iter()
+        .collect::<String>()
+        .parse::<u32>()
+        .expect("Unable to parse part number");
+
+    for i in 0..part.len() {
+        schematic.insert((row, col - i), Point::Part(number));
+    }
+    part.clear();
+}
+
+pub fn day03(input: Vec<String>) -> (String, String) {
+    let mut schematic = HashMap::new();
+
+    // Populate schematic
+    for (row, line) in input.iter().enumerate() {
+        let mut part = Vec::new();
+        for (col, c) in line.chars().enumerate() {
+            match c {
+                '.' => {
+                    if !part.is_empty() {
+                        process_part(&mut schematic, row, col - 1, &mut part);
+                    }
+                    schematic.insert((row, col), Point::Period);
+                }
+                _ => {
+                    if c.is_numeric() {
+                        part.push(c);
+                    } else {
+                        if !part.is_empty() {
+                            process_part(&mut schematic, row, col - 1, &mut part);
+                        }
+                        schematic.insert((row, col), Point::Symbol(c));
+                    }
+                }
+            }
+        }
+
+        // Handle number at the end of a row
+        if !part.is_empty() {
+            process_part(&mut schematic, row, line.len() - 1, &mut part);
+        }
+    }
+
+    // Find symbols in the schematic and sum adjacent parts
+    let mut part_1_parts = Vec::new();
+    let mut part_2_sum = 0;
+    for ((row, col), point) in &schematic {
+        if let Point::Symbol(s) = point {
+            let mut adjacent_parts = HashSet::new();
+            for r in row - 1..row + 2 {
+                for c in col - 1..col + 2 {
+                    match schematic.get(&(r, c)) {
+                        Some(Point::Part(n)) => {
+                            adjacent_parts.insert(*n);
+                        }
+                        _ => (),
+                    }
+                }
+            }
+            for part in &adjacent_parts {
+                part_1_parts.push(*part);
+            }
+            if *s == '*' && adjacent_parts.len() == 2 {
+                part_2_sum += adjacent_parts.iter().product::<u32>();
             }
         }
     }
-    false
+
+    (
+        format!("{}", part_1_parts.iter().sum::<u32>()),
+        format!("{}", part_2_sum),
+    )
 }
 
 #[cfg(test)]
@@ -64,27 +100,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day03_1() {
-        assert_eq!(
-            day03(vec![
-                "467..114..".to_string(),
-                "...&......".to_string(),
-                "..35..633.".to_string(),
-                "......#...".to_string(),
-                "617*......".to_string(),
-                ".....+.58.".to_string(),
-                "..592.....".to_string(),
-                "......755.".to_string(),
-                "...$.*....".to_string(),
-                ".664.598..".to_string(),
-            ])
-            .0,
-            "4361"
-        )
-    }
-
-    #[test]
-    fn day03_2() {
-        assert_eq!(day03(vec!["".to_string()]).1, "ANSWER_2")
+    fn day03_1_2() {
+        let result = day03(vec![
+            "467..114..".to_string(),
+            "...*......".to_string(),
+            "..35..633.".to_string(),
+            "......#...".to_string(),
+            "617*......".to_string(),
+            ".....+.58.".to_string(),
+            "..592.....".to_string(),
+            "......755.".to_string(),
+            "...$.*....".to_string(),
+            ".664.598..".to_string(),
+        ]);
+        assert_eq!(result.0, "4361");
+        assert_eq!(result.1, "467835");
     }
 }
