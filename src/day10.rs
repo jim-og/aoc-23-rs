d2cf53d4adaf2ab5edad4e654cd1cc17f7450bae
Jim Gartland <jim@danube.io>
2024-10-28 19:22:19 +0000

    Day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+type Point = (i32, i32);
+
 enum Heading {
     North,
     East,
@@ -8,7 +10,7 @@ enum Heading {
 }
 
 impl Heading {
-    pub fn from(&self, loc: (i32, i32)) -> (i32, i32) {
+    pub fn from(&self, loc: Point) -> Point {
         match self {
             Heading::North => (loc.0 - 1, loc.1),
             Heading::East => (loc.0, loc.1 + 1),
@@ -30,11 +32,11 @@ enum Route {
     Start,
 }
 
-struct Point {
+struct Pipe {
     route: Route,
 }
 
-impl Point {
+impl Pipe {
     pub fn new(symbol: char) -> Self {
         let route = match symbol {
             '|' => Route::NorthAndSouth,
@@ -79,12 +81,23 @@ impl Point {
     }
 }
 
+/// Shoelace Formula (https://en.wikipedia.org/wiki/Shoelace_formula)
+/// The starting point must be present at the start and the end of the outline
+fn shoelace(outline: &[Point]) -> usize {
+    let sum = outline.windows(2).fold(0, |acc, matrix| {
+        acc + (matrix[0].0 * matrix[1].1) - (matrix[1].0 * matrix[0].1)
+    });
+    // If the points are labeled sequentially in the counterclockwise direction, then the area is positive,
+    // if they are labeled in the clockwise direction, the area will be negative.
+    (sum.abs() / 2) as usize
+}
+
 pub fn day10(input: Vec<String>) -> (String, String) {
     let mut points = HashMap::new();
-    let mut start: Option<(i32, i32)> = None;
+    let mut start: Option<Point> = None;
     for (row, line) in input.iter().enumerate() {
         for (col, c) in line.chars().enumerate() {
-            let point = Point::new(c);
+            let point = Pipe::new(c);
             if point.route == Route::Start {
                 start = Some((row as i32, col as i32));
             }
@@ -107,42 +120,68 @@ pub fn day10(input: Vec<String>) -> (String, String) {
     let mut steps = 1;
     let mut loc = heading.from(start.unwrap());
     let mut point = points.get(&loc).expect("No point at location");
+    let mut outline = Vec::from([start.unwrap(), loc]);
     while point.route != Route::Start {
         heading = point.get_heading(&heading).expect("Heading not valid");
         loc = heading.from(loc);
         point = points.get(&loc).expect("No point at location");
         steps += 1;
+        outline.push(loc);
     }
+    let area: usize = shoelace(&outline);
+    // Pick's Theorem (https://en.wikipedia.org/wiki/Pick%27s_theorem)
+    let interior_points = area - (outline.len() - 1) / 2 + 1;
 
-    (format!("{}", steps / 2), "".to_string())
+    (format!("{}", steps / 2), format!("{}", interior_points))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::parser;
+    use test_case::test_case;
 
-    #[test]
-    fn example_both() {
-        let result_1 = day10(parser::test_input(
-            "-L|F7
-            7S-7|
-            L|7||
-            -L-J|
-            L|-JF",
-        ));
-        assert_eq!(result_1.0, "4");
-        assert_eq!(result_1.1, "");
-
-        let result_2 = day10(parser::test_input(
-            "7-F7-
-            .FJ|7
-            SJLL7
-            |F--J
-            LJ.LJ",
-        ));
-        assert_eq!(result_2.0, "8");
-        assert_eq!(result_2.1, "");
+    #[test_case("
+        -L|F7
+        7S-7|
+        L|7||
+        -L-J|
+        L|-JF
+        ","4","1";"one")]
+    #[test_case("
+        7-F7-
+        .FJ|7
+        SJLL7
+        |F--J
+        LJ.LJ
+        ","8","1"; "two")]
+    #[test_case("
+        ...........
+        .S-------7.
+        .|F-----7|.
+        .||.....||.
+        .||.....||.
+        .|L-7.F-J|.
+        .|..|.|..|.
+        .L--J.L--J.
+        ...........
+        ","23","4"; "three")]
+    #[test_case("
+        FF7FSF7F7F7F7F7F---7
+        L|LJ||||||||||||F--J
+        FL-7LJLJ||||||LJL-77
+        F--JF--7||LJLJ7F7FJ-
+        L---JF-JLJ.||-FJLJJ7
+        |F|F-JF---7F7-L7L|7|
+        |FFJF7L7F-JF7|JL---7
+        7-L-JL7||F7|L7F-7F7|
+        L.L7LFJ|||||FJL7||LJ
+        L7JLJL-JLJLJL--JLJ.L
+        ","80","10"; "four")]
+    fn example_both(input: &str, part_1: &str, part_2: &str) {
+        let result_1 = day10(parser::test_input(input));
+        assert_eq!(result_1.0, part_1);
+        assert_eq!(result_1.1, part_2);
     }
 
     #[test]
@@ -150,6 +189,6 @@ mod tests {
         let input = parser::load_input(10);
         let result = day10(input);
         assert_eq!(result.0, "6927");
-        assert_eq!(result.1, "");
+        assert_eq!(result.1, "467");
     }
 }
