42c1a723a02dbe467b8ab1dbca604e6b019e036b
Jim Gartland <jim@danube.io>
2024-10-31 16:18:50 +0000

    Refactor Day 13 with traits

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,109 @@
+trait Symmetrical {
+    fn is_symmetrical(&self, index: usize) -> bool;
+}
+
+impl Symmetrical for Vec<char> {
+    fn is_symmetrical(&self, index: usize) -> bool {
+        let min_width = index.min(self.len() - index);
+        let left = &self[index - min_width..index];
+        let right = &mut self.to_owned()[index..index + min_width];
+        right.reverse();
+        left == right
+    }
+}
+
+type Pattern = Vec<Vec<char>>;
+
+trait Mirror {
+    fn get_candidates(&self) -> Vec<usize>;
+    fn get_mirror(
+        &self,
+        prev: (Option<usize>, Option<usize>),
+    ) -> Option<(Option<usize>, Option<usize>)>;
+    fn get_clean_mirror(&self) -> Option<(Option<usize>, Option<usize>)>;
+    fn transpose(&self) -> Pattern;
+}
+
+impl Mirror for Pattern {
+    fn get_candidates(&self) -> Vec<usize> {
+        let mut candidates = Vec::new();
+        let width = self.first().expect("Pattern empty").len();
+
+        // Scan the first row for candidates
+        if let Some(line) = self.first() {
+            for i in 1..width {
+                if line.is_symmetrical(i) {
+                    candidates.push(i);
+                }
+            }
+        }
+
+        // Scan each row, eliminating invalid candidates
+        for (index, line) in self.iter().enumerate() {
+            if index == 0 {
+                continue;
+            }
+            if candidates.is_empty() {
+                break;
+            }
+            candidates = candidates
+                .into_iter()
+                .filter(|c| line.is_symmetrical(*c))
+                .collect::<Vec<usize>>();
+        }
+
+        candidates
+    }
+
+    fn get_mirror(
+        &self,
+        prev: (Option<usize>, Option<usize>),
+    ) -> Option<(Option<usize>, Option<usize>)> {
+        // Search for vertical mirror line
+        for candidate in self.get_candidates() {
+            let mirror = (Some(candidate), None);
+            if mirror != prev {
+                return Some(mirror);
+            }
+        }
+
+        // Search for horizontal mirror line
+        for candidate in self.transpose().get_candidates() {
+            let mirror = (None, Some(candidate));
+            if mirror != prev {
+                return Some(mirror);
+            }
+        }
+
+        None
+    }
+
+    fn get_clean_mirror(&self) -> Option<(Option<usize>, Option<usize>)> {
+        let prev = self.get_mirror((None, None)).expect("No mirror found");
+        let width = self.first().expect("Expected pattern to have a row").len();
+        let height = self.len();
+        for i in 0..height {
+            for j in 0..width {
+                let mut clean_pattern = self.to_owned();
+                clean_pattern[i][j] = match clean_pattern[i][j] {
+                    '.' => '#',
+                    '#' => '.',
+                    _ => unreachable!(),
+                };
+                match clean_pattern.get_mirror(prev) {
+                    None => continue,
+                    Some(mirror) => return Some(mirror),
+                }
+            }
+        }
+        None
+    }
+
+    fn transpose(&self) -> Pattern {
+        inner_transpose(self)
+    }
+}
+
 fn get_patterns(input: &[String]) -> Vec<Vec<Vec<char>>> {
     let mut patterns = Vec::new();
     let mut pattern = Vec::new();
@@ -13,92 +119,7 @@ fn get_patterns(input: &[String]) -> Vec<Vec<Vec<char>>> {
     patterns
 }
 
-fn get_candidates(pattern: &[Vec<char>]) -> Vec<usize> {
-    let mut candidates = Vec::new();
-    let width = pattern.first().expect("Pattern empty").len();
-
-    // Scan the first row for candidates
-    if let Some(line) = pattern.first() {
-        for i in 1..width {
-            if is_symmetrical(line, i) {
-                candidates.push(i);
-            }
-        }
-    }
-
-    // Scan each row, eliminating invalid candidates
-    for (index, line) in pattern.iter().enumerate() {
-        if index == 0 {
-            continue;
-        }
-        if candidates.is_empty() {
-            break;
-        }
-        candidates = candidates
-            .into_iter()
-            .filter(|c| is_symmetrical(line, *c))
-            .collect::<Vec<usize>>();
-    }
-
-    candidates
-}
-
-fn get_mirror(
-    pattern: &[Vec<char>],
-    prev: (Option<usize>, Option<usize>),
-) -> Option<(Option<usize>, Option<usize>)> {
-    // Search for vertical mirror line
-    for candidate in get_candidates(pattern) {
-        let mirror = (Some(candidate), None);
-        if mirror != prev {
-            return Some(mirror);
-        }
-    }
-
-    // Search for horizontal mirror line
-    for candidate in get_candidates(&transpose(pattern)) {
-        let mirror = (None, Some(candidate));
-        if mirror != prev {
-            return Some(mirror);
-        }
-    }
-
-    None
-}
-
-fn get_clean_mirror(pattern: &[Vec<char>]) -> Option<(Option<usize>, Option<usize>)> {
-    let prev = get_mirror(pattern, (None, None)).expect("No mirror found");
-    let width = pattern
-        .first()
-        .expect("Expected pattern to have a row")
-        .len();
-    let height = pattern.len();
-    for i in 0..height {
-        for j in 0..width {
-            let mut clean_pattern = pattern.to_owned();
-            clean_pattern[i][j] = match clean_pattern[i][j] {
-                '.' => '#',
-                '#' => '.',
-                _ => unreachable!(),
-            };
-            match get_mirror(&clean_pattern, prev) {
-                None => continue,
-                Some(mirror) => return Some(mirror),
-            }
-        }
-    }
-    None
-}
-
-fn is_symmetrical(line: &[char], index: usize) -> bool {
-    let min_width = index.min(line.len() - index);
-    let left = &line[index - min_width..index];
-    let right = &mut line.to_owned()[index..index + min_width];
-    right.reverse();
-    left == right
-}
-
-fn transpose<T>(v: &[Vec<T>]) -> Vec<Vec<T>>
+fn inner_transpose<T>(v: &[Vec<T>]) -> Vec<Vec<T>>
 where
     T: Clone,
 {
@@ -112,7 +133,7 @@ fn part_1(patterns: &[Vec<Vec<char>>]) -> usize {
     let scores = patterns
         .iter()
         .map(
-            |pattern| match get_mirror(pattern, (None, None)).expect("No mirror found") {
+            |pattern| match pattern.get_mirror((None, None)).expect("No mirror found") {
                 (Some(s), None) => s,
                 (None, Some(s)) => s * 100,
                 _ => unimplemented!(),
@@ -127,7 +148,7 @@ fn part_2(patterns: &[Vec<Vec<char>>]) -> usize {
     let scores = patterns
         .iter()
         .map(
-            |pattern| match get_clean_mirror(pattern).expect("No mirror found") {
+            |pattern| match pattern.get_clean_mirror().expect("No mirror found") {
                 (Some(s), None) => s,
                 (None, Some(s)) => s * 100,
                 _ => unimplemented!(),
