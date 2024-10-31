ae3e19787de3035d244e74fafbb84c115d9d0ea4
Jim Gartland <jim@danube.io>
2024-10-31 15:52:05 +0000

    Day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -1,4 +1,4 @@
-pub fn solve(input: &[String]) -> usize {
+fn get_patterns(input: &[String]) -> Vec<Vec<Vec<char>>> {
     let mut patterns = Vec::new();
     let mut pattern = Vec::new();
     for line in input {
@@ -10,22 +10,10 @@ pub fn solve(input: &[String]) -> usize {
         pattern.push(line.chars().collect::<Vec<_>>());
     }
     patterns.push(pattern);
-
-    let mut part_1 = Vec::new();
-    for pattern in patterns {
-        if let Some(vertical_line) = get_mirror_index(&pattern) {
-            part_1.push(vertical_line);
-        } else if let Some(horizontal_line) = get_mirror_index(&transpose(&pattern)) {
-            part_1.push(100 * horizontal_line);
-        } else {
-            panic!("No line of reflection found");
-        }
-    }
-
-    part_1.iter().sum::<usize>()
+    patterns
 }
 
-fn get_mirror_index(pattern: &Vec<Vec<char>>) -> Option<usize> {
+fn get_candidates(pattern: &[Vec<char>]) -> Vec<usize> {
     let mut candidates = Vec::new();
     let width = pattern.first().expect("Pattern empty").len();
 
@@ -38,7 +26,7 @@ fn get_mirror_index(pattern: &Vec<Vec<char>>) -> Option<usize> {
         }
     }
 
-    // Scan each row
+    // Scan each row, eliminating invalid candidates
     for (index, line) in pattern.iter().enumerate() {
         if index == 0 {
             continue;
@@ -52,22 +40,65 @@ fn get_mirror_index(pattern: &Vec<Vec<char>>) -> Option<usize> {
             .collect::<Vec<usize>>();
     }
 
-    if candidates.len() == 1 {
-        return Some(*candidates.first().expect("Expected a candidate"));
-    } else {
-        None
-    }
+    candidates
 }
 
-fn is_symmetrical(line: &Vec<char>, index: usize) -> bool {
+fn get_mirror(
+    pattern: &[Vec<char>],
+    prev: (Option<usize>, Option<usize>),
+) -> Option<(Option<usize>, Option<usize>)> {
+    // Search for vertical mirror line
+    for candidate in get_candidates(pattern) {
+        let mirror = (Some(candidate), None);
+        if mirror != prev {
+            return Some(mirror);
+        }
+    }
+
+    // Search for horizontal mirror line
+    for candidate in get_candidates(&transpose(pattern)) {
+        let mirror = (None, Some(candidate));
+        if mirror != prev {
+            return Some(mirror);
+        }
+    }
+
+    None
+}
+
+fn get_clean_mirror(pattern: &[Vec<char>]) -> Option<(Option<usize>, Option<usize>)> {
+    let prev = get_mirror(pattern, (None, None)).expect("No mirror found");
+    let width = pattern
+        .first()
+        .expect("Expected pattern to have a row")
+        .len();
+    let height = pattern.len();
+    for i in 0..height {
+        for j in 0..width {
+            let mut clean_pattern = pattern.to_owned();
+            clean_pattern[i][j] = match clean_pattern[i][j] {
+                '.' => '#',
+                '#' => '.',
+                _ => unreachable!(),
+            };
+            match get_mirror(&clean_pattern, prev) {
+                None => continue,
+                Some(mirror) => return Some(mirror),
+            }
+        }
+    }
+    None
+}
+
+fn is_symmetrical(line: &[char], index: usize) -> bool {
     let min_width = index.min(line.len() - index);
     let left = &line[index - min_width..index];
-    let right = &mut line.clone()[index..index + min_width];
+    let right = &mut line.to_owned()[index..index + min_width];
     right.reverse();
     left == right
 }
 
-fn transpose<T>(v: &Vec<Vec<T>>) -> Vec<Vec<T>>
+fn transpose<T>(v: &[Vec<T>]) -> Vec<Vec<T>>
 where
     T: Clone,
 {
@@ -77,11 +108,42 @@ where
         .collect()
 }
 
-pub fn day13(input: Vec<String>) -> (String, String) {
-    let part_1 = solve(&input);
-    (format!("{}", part_1), format!("{}", 0))
+fn part_1(patterns: &[Vec<Vec<char>>]) -> usize {
+    let scores = patterns
+        .iter()
+        .map(
+            |pattern| match get_mirror(pattern, (None, None)).expect("No mirror found") {
+                (Some(s), None) => s,
+                (None, Some(s)) => s * 100,
+                _ => unimplemented!(),
+            },
+        )
+        .collect::<Vec<_>>();
+
+    scores.iter().sum::<usize>()
 }
 
+fn part_2(patterns: &[Vec<Vec<char>>]) -> usize {
+    let scores = patterns
+        .iter()
+        .map(
+            |pattern| match get_clean_mirror(pattern).expect("No mirror found") {
+                (Some(s), None) => s,
+                (None, Some(s)) => s * 100,
+                _ => unimplemented!(),
+            },
+        )
+        .collect::<Vec<_>>();
+
+    scores.iter().sum::<usize>()
+}
+
+pub fn day13(input: Vec<String>) -> (String, String) {
+    let patterns = get_patterns(&input);
+    let part_1 = part_1(&patterns);
+    let part_2 = part_2(&patterns);
+    (format!("{}", part_1), format!("{}", part_2))
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -106,12 +168,15 @@ mod tests {
         ..##..###
         #....#..#
         ",
-        405
+        "405",
+        "400"
         ;"1"
     )]
-    fn example(input: &str, answer: usize) {
+    fn example(input: &str, part_1: &str, part_2: &str) {
         let data = parser::test_input(input);
-        assert_eq!(solve(&data), answer);
+        let result = day13(data);
+        assert_eq!(result.0, part_1);
+        assert_eq!(result.1, part_2);
     }
 
     #[test]
@@ -119,6 +184,6 @@ mod tests {
         let input = parser::load_input(13);
         let result = day13(input);
         assert_eq!(result.0, "33975");
-        assert_eq!(result.1, "0");
+        assert_eq!(result.1, "29083");
     }
 }
