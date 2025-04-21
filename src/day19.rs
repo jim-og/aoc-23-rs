de0f4f2f034a091f71474ec160f7c0322e9eaac8
Jim Gartland <jim@danube.io>
2025-04-21 13:55:41 +0100

    Day 19 setup

---
+++ src/day19.rs
@@ -0,0 +1,29 @@
+#[aoc_generator(day19)]
+fn parse(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day19, part1)]
+fn part1(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day19, part2)]
+fn part2(input: &str) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ pub mod day15;
 pub mod day16;
 pub mod day17;
 pub mod day18;
+pub mod day19;
 pub mod parser;
 
 aoc_lib! { year = 2023 }
